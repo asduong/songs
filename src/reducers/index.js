cec8d71b8468ac5aff40d01be10c81ca529896b2
Andrew Duong <duongandrews@gmail.com>
2020-03-03 00:23:05 -0500

    Added Screenshots

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,12 @@ const songsReducer = () => {
     { title: "No Scrubs", duration: "4:05" },
     { title: "Macarena", duration: "2:30" },
     { title: "All Star", duration: "3:15" },
-    { title: "I want it that way", duration: "1:45" }
+    { title: "I want it that way", duration: "2:35" },
+    { title: "Poker Face", duration: "3:32" },
+    { title: "Symphony No. 8", duration: "4:31" },
+    { title: "Choral Fantasy", duration: "4:45" },
+    { title: "Fidelio", duration: "3:10" },
+    { title: "Für Elise", duration: "3:23" }
   ];
 };
 
